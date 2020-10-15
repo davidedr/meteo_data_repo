478422095abddc0022c671796a1bd3844fe4257e
DavideDR <d.dr@libero.it>
2020-10-15 17:13:44 +0200

    Adds simple Python client

---
+++ simple_client.py
@@ -0,0 +1,22 @@
+# Simple Python client
+import requests
+import json
+
+data_json = {
+    "location_id": 1,
+    "wind_speed_knots": 20,
+    "wind_direction_deg": 21,
+    "barometric_pressure_hPa": 22.1,
+    "rain_today_mm": 23,
+    "rain_rate_mmph": 24.1,
+    "temperature_cels": 25.1,
+    "rel_humidity": 0.26,
+    "uv_index": 27.1,
+    "heat_index_cels": 28.1
+}
+
+headers = {'Content-Type': 'application/json; charset=utf-8'}
+response = requests.post('http://localhost:8080/api/meteo_data', headers = headers, json = data_json)
+print("response")
+print(response.status_code)
+print(response)
